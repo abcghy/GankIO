508e5574c80922a42223a3b9450e4491ab574dcf
gaohuiyu <beatbox_gao@hotmail.com>
2017-12-03 20:06:13 +0800

    update .gitignore

--- adapter/build/generated/source/apt/debug/com/github/markzhai/recyclerview/BR.java
+++
@@ -1,7 +0,0 @@
-package com.github.markzhai.recyclerview;
-
-public class BR {
-        public static int _all = 0;
-        public static int item = 1;
-        public static int presenter = 2;
-}
--- adapter/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="25c2517c-4392-4f3f-ae07-75408584cf7d")
-public class DataBindingInfo {}
